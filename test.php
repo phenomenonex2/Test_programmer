fece23a08d26599640e2e14b021f40d392b4e782
phenomenonex2 <pack_soi4@hotmail.com>
2020-07-07 16:43:07 +0700

    Update test.php

--- test.php
+++ test.php
@@ -1,9 +1,13 @@
 /* ------- Example 1 ------- */
 <?php  
-for($i=0;$i<=5;$i++){  
-for($j=1;$j<=$i;$j++){  
-echo "o ";  
-}  
-echo "<br>";  
-}  
+  for($i=0;$i<=5;$i++){  
+    for($j=1;$j<=$i;$j++){  
+      if($j<=(5-$i)){ 
+        echo " "." ";      
+      }else {
+        echo "* ";
+      }
+    }  
+    echo "<br>";  
+   }  
 ?>  
